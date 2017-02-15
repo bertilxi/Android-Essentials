515f1b97904ab3692a5f0371c92c3cf8937898dd
Fernando Berti <bertilxi@gmail.com>
2017-02-15 14:48:10 -0300

    clase 2 terminada

---
+++ app/src/main/java/folderit/net/ejemplos/clase2/TimepickerActivity.java
@@ -0,0 +1,16 @@
+package folderit.net.ejemplos.clase2;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import folderit.net.ejemplos.R;
+
+public class TimepickerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.timepicker);
+    }
+}
